2e36c284b297c9929917b6f3a74243a0b5c6ad04
pphein <pph.hein87@gmail.com>
2024-02-07 07:36:54 +0630

    initial

---
+++ src/lib/db.js
@@ -0,0 +1,21 @@
+import mongoose, { connection } from "mongoose";
+
+async function dbConnect() {
+    if (connection.isConnected) {
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI, {
+            userNewUrlParse: true,
+            userUnifiedTopology: true,
+        });
+
+        connection.isConnected = db.connections[0].readyState;
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB: ', error.message);
+    }
+}
+
+export default dbConnect;
