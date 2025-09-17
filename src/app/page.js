4a5bc1da4ab54ce4d1e1fdc2dd4a16b95702264e
pphein <pph.hein87@gmail.com>
2025-09-17 06:43:36 +0630

    Added loading spinner

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import {RotatingLines} from "react-loader-spinner";
 import Hero from "@/components/Hero";
 import PostCard from "@/components/PostCard";
-import Image from "next/image";
 
-export async function fetchPosts() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3001'
-  const res = await fetch(`${baseUrl}/api/post`, { cache: 'no-store' })
-  return res.json()
-}
+export default function Home() {
+  const [posts, setPosts] = useState([]);
 
-export default async function Home() {
-  const posts = await fetchPosts()
+  useEffect(() => {
+    async function loadPosts() {
+      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3001';
+      const res = await fetch(`${baseUrl}/api/post`, { cache: 'no-store' });
+      const data = await res.json();
+      setPosts(data);
+    }
+    loadPosts();
+  }, []);
+
   return (
     <main className="max-w-screen-sm m-auto">
       <Hero />
       <div>
-        {/* {posts?.length > 0 && <h2 className="text-center">The Blog Spot</h2>} */}
         {
           posts?.length > 0
             ? posts.map((post) =>
               (<PostCard key={post._id} post={post} />)
             )
-            : (<h3>Post will be loaded soon</h3>)
+            : (
+            <div className="flex items-center justify-center">
+                <RotatingLines
+                  strokeColor="grey"
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  width="96"
+                  visible={true}
+                />
+              </div>
+          )
         }
       </div>
     </main>
